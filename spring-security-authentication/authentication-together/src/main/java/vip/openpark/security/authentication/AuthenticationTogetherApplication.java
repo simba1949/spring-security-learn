90c08b72b0c71a8a41c8fa9bba106c517e384cdf
openpark <openpark.vip@outlook.com>
2024-01-30 23:32:44 +0800

    build：重整目录结构

--- spring-security-authentication/authentication-together/src/main/java/vip/openpark/security/authentication/AuthenticationTogetherApplication.java
+++ spring-security-authentication/authentication-together/src/main/java/vip/openpark/security/authentication/AuthenticationTogetherApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
+ * <div>
+ *     <title>前后端不分离的认证</title>
+ * </div>
+ *
  * @author anthony
  * @since 2024/1/25 12:13
  */
 @MapperScan("vip.openpark.security.authentication.mapper")
 @SpringBootApplication
-public class AuthenticationApplication {
+public class AuthenticationTogetherApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationApplication.class, args);
+		SpringApplication.run(AuthenticationTogetherApplication.class, args);
 	}
 }
