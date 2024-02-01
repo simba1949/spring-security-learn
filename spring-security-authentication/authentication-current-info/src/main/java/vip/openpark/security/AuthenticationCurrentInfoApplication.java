d3050710f51924dcd411ed95ae031b6ef202fc86
anthony <anthony1949@zto.com>
2024-02-01 09:42:15 +0800

    feature：spring security 获取当前登录用户信息

---
+++ spring-security-authentication/authentication-current-info/src/main/java/vip/openpark/security/AuthenticationCurrentInfoApplication.java
@@ -0,0 +1,15 @@
+package vip.openpark.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author anthony
+ * @version 2024/2/1 9:31
+ */
+@SpringBootApplication
+public class AuthenticationCurrentInfoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationCurrentInfoApplication.class, args);
+	}
+}
