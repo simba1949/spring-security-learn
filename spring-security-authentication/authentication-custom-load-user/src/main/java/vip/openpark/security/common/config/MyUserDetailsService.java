8484f3e88974036e186cd9ba42c9fa0463f41a0f
openpark <openpark.vip@outlook.com>
2024-02-04 23:13:46 +0800

    feature：自定义加载用户信息

---
+++ spring-security-authentication/authentication-custom-load-user/src/main/java/vip/openpark/security/common/config/MyUserDetailsService.java
@@ -0,0 +1,86 @@
+package vip.openpark.security.common.config;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import vip.openpark.security.domain.UserDO;
+import vip.openpark.security.service.UserService;
+
+/**
+ * <div>
+ *     接口 {@link UserDetailsService} 用于通过用户名查询用户信息及其权限;
+ *     将获取到的用户信息及其权限，封装到 {@link UserDetails} 中
+ * </div>
+ *
+ * @author anthony
+ * @version 2024/1/25 13:48
+ */
+@Slf4j
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+	@Resource
+	private UserService userService;
+	
+	/**
+	 * 根据用户名查询用户信息及权限，封装到 {@link UserDetails} 中
+	 *
+	 * <div>
+	 *     spring security 5.0（含）之前，加密器使用方式为
+	 *     1:先声明成 PasswordEncoder bean，参考自定义配置类 {@link PasswordEncoderConfig}
+	 *     1.1:自定义配置类型使用 {@link BCryptPasswordEncoder} 作为加密器
+	 *     2:然后在注入使用
+	 * </div>
+	 * <div>
+	 *     spring security 5.0 之后，加密器使用方式为：{@link PasswordEncoderFactories}，用于选择加密器
+	 *     使用 {@code PasswordEncoderFactories.createDelegatingPasswordEncoder()} 来创建密码加密器
+	 * </div>
+	 *
+	 * @param username the username identifying the user whose data is required.
+	 * @return the user data required.
+	 * @throws UsernameNotFoundException if the user could not be found.
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.info("登录用户名：{}", username);
+		
+		// 根据用户名去数据库中查询用户信息包括密码
+		UserDO userDO = userService.get(username);
+		// 判断用户账号是否【存在】，如果不存在直接报错
+		if (userDO == null) {
+			throw new UsernameNotFoundException("用户不存在");
+		}
+		
+		String plaintextPassword = userDO.getPassword();
+		log.info("从数据库中查询的明文密码为：{}", plaintextPassword);
+		// 这里理论上不应该使用 {@code PasswordEncoderFactories.createDelegatingPasswordEncoder()}
+		// 应该使用的是数据库中的密码，数据库中的密码是加密后的密码
+		// 这里要和配置的 {@code PasswordEncoder} 加密方式一致
+		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		String encodePassword = passwordEncoder.encode(plaintextPassword);
+		log.info("从数据库中查询的密文密码为：{}", encodePassword);
+		
+		// 根据用户名去数据查询用户的信息
+		// 判断用户账号是否【启用】，true 表示启用，false 表示禁用
+		boolean enabledFlag = true;
+		// 判断用户账号是否【没有过期】，true 表示不过期，false 表示过期
+		boolean accountNonExpiredFlag = true;
+		// 判断用户账号密码是否【没有过期】，true 表示未过期，false 表示过期
+		boolean credentialsNonExpiredFlag = true;
+		// 判断用户账号是否【没有冻结】，true 表示没有冻结，false 表示已冻结
+		boolean accountNonLockedFlag = true;
+		
+		// 第一个参数表示用户名，第二个参数表示加密后的密码，最后一个参数表示该用户拥有哪些角色（角色对应权限）
+		return new User(
+			username, encodePassword,
+			enabledFlag, accountNonExpiredFlag, credentialsNonExpiredFlag, accountNonLockedFlag,
+			AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+	}
+}
--- spring-security-authentication/authentication-together/src/main/java/vip/openpark/security/mapper/RolePermissionDOMapper.java
+++
@@ -1,7 +0,0 @@
-package vip.openpark.security.mapper;
-
-import tk.mybatis.mapper.common.Mapper;
-import vip.openpark.security.domain.RolePermissionDO;
-
-public interface RolePermissionDOMapper extends Mapper<RolePermissionDO> {
-}
