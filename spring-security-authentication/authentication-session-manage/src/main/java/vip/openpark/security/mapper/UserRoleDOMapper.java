65b1cc85b05d3c8b231555adb68334d345e7bf92
openpark <openpark.vip@outlook.com>
2024-02-05 08:29:33 +0800

    feature：会话管理删除非必要代码

--- spring-security-authentication/authentication-session-manage/src/main/java/vip/openpark/security/mapper/UserRoleDOMapper.java
+++
@@ -1,7 +0,0 @@
-package vip.openpark.security.mapper;
-
-import tk.mybatis.mapper.common.Mapper;
-import vip.openpark.security.domain.UserRoleDO;
-
-public interface UserRoleDOMapper extends Mapper<UserRoleDO> {
-}
